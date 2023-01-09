415b4f9708b54dbe18d7b406cadcf5064fd372e3
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2023-01-09 16:03:02 -0300

    add firebase auth service

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,14 +1,14 @@
 import 'package:financy_app/features/sing_in/sing_in_controller.dart';
 import 'package:financy_app/features/sing_up/sing_up_controller.dart';
-import 'package:financy_app/services/mock_auth_service.dart';
 import 'package:get_it/get_it.dart';
 
 import 'services/auth_service.dart';
+import 'services/firebase_auth_service.dart';
 
 final locator = GetIt.instance;
 
 void setupDependencies() {
-  locator.registerLazySingleton<AuthService>(() => MockAuthService());
+  locator.registerLazySingleton<AuthService>(() => FirebaseAuthService());
 
   locator.registerFactory<SingInController>(
       () => SingInController(locator.get<AuthService>()));
